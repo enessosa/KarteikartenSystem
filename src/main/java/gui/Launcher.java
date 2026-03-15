63e2cfe2e581206505fc2c927d849a824bac1607
Enes Sadi <e.sadi@hm.edu>
2026-03-15 02:36:25 +0100

    Trying to fix the Fat-Jar

--- src/main/java/gui/Launcher.java
+++ src/main/java/gui/Launcher.java
@@ -1,9 +1,21 @@
 package gui;
 
+import helper.DAO.CardDAO;
+import helper.DAO.DeckDAO;
+import helper.DatabaseManager;
 import javafx.application.Application;
+
+import java.sql.SQLException;
 
 public class Launcher {
     public static void main(String[] args) {
+        try {
+            DatabaseManager.getConnection();
+            DeckDAO.createDeckDB();
+            CardDAO.createCardDB();
+        } catch (SQLException e) {
+            throw new RuntimeException("Datenbank konnte nicht initialisiert werden", e);
+        }
         Application.launch(GUI.class, args);
     }
 }
